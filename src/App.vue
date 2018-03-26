d3ec869952d18293dcea20fb2df489bdba36ba6f
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-03-26 13:19:19 -0500

    flesh out app

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,52 @@
 <template>
-  <div id="app">
+  <div id="app" v-bind:style="dynamicStyle">
     <!-- <img src="./assets/logo.png"> -->
-    <router-view/>
+    <h1>Fortune</h1>
+    <router-view :themeColors="themeColors"/>
   </div>
 </template>
 
 <script>
+
+import {
+  pullRandom,
+  colors,
+} from './groups';
+
+const color1 = pullRandom(colors);
+const color2 = pullRandom(colors);
+
 export default {
   name: 'app',
+  computed: {
+    themeColors() {
+      return color1;
+    },
+    dynamicStyle() {
+      return {
+        background: color1.hues[3],
+        color: color2.hues[1],
+      };
+    },
+  },
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 75px 0;
+  height: 100vh;
+  width: 100vw;
+  background: blue;
   font-family: 'Josefin Sans', cursive;
 }
 
@@ -33,8 +61,8 @@ h1 {
 }
 
 hr {
-    border: solid 1px;
+    border: solid 1.5px;
     width: 100%;
-    margin: 15px 0;
+    margin: 10px 0;
 }
 </style>
