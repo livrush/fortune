093ec8ff6e006e7abe62157e2e49a08dce49ddcf
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-03-26 13:18:40 -0500

    add colors and helper functions

--- src/groups.js
+++ src/groups.js
@@ -1,10 +1,40 @@
-const verbs = [
+export function pullRandom(array) {
+  const index = Math.floor(Math.random() * array.length);
+  return array[index];
+}
+
+export const luck = [
+  'Very Good',
+  'Good',
+  'Normal',
+  'Bad',
+  'Very Bad',
+];
+
+export const verbs = [
   'Beware',
   'Embrace',
   'Consider',
 ];
 
-const groups = [
+export const actions = [
+  'Mind',
+  'Embody',
+  'Accommodate',
+  'Exceed',
+  'Pamper',
+  'Eclipse',
+  'Transcend',
+  'Surrender',
+  'Subdue',
+  'Vanquish',
+  'Surpass',
+  'Dispute',
+  'Apprehend',
+  'Believe',
+];
+
+export const groups = [
   'family',
   'friends',
   'lovers',
@@ -13,3 +43,122 @@ const groups = [
   'authority',
   'animals',
 ];
+
+export const feelings = [
+  'joy',
+  'fear',
+  'life',
+  'death',
+];
+
+export const colors = [
+  {
+    hues: [
+      '#C9B6BE',
+      '#8E8C97',
+      '#5F6062',
+      '#24232A',
+    ],
+    name: 'Infinity',
+  },
+  {
+    hues: [
+      '#FDD692',
+      '#B99C6B',
+      '#8D775F',
+      '#4D4134',
+    ],
+    name: 'Cashmere',
+  },
+  {
+    hues: [
+      '#AE8799',
+      '#94778B',
+      '#4E344A',
+      '#270722',
+    ],
+    name: 'Soul',
+  },
+  {
+    hues: [
+      '#FFAFAF',
+      '#C95D63',
+      '#803C40',
+      '#6A0000',
+    ],
+    name: 'Cranberry',
+  },
+  {
+    hues: [
+      '#FF5D48',
+      '#FF3A20',
+      '#BA2B18',
+      '#931621',
+    ],
+    name: 'Brick',
+  },
+  {
+    hues: [
+      '#FF7733',
+      '#E36414',
+      '#A6490F',
+      '#7C370B',
+    ],
+    name: 'Orange',
+  },
+  {
+    hues: [
+      '#FFDD7A',
+      '#FFCA2E',
+      '#BA9322',
+      '#745C15',
+    ],
+    name: 'Bronze',
+  },
+  {
+    hues: [
+      '#F3FF92',
+      '#C6CA53',
+      '#739130',
+      '#103900',
+    ],
+    name: 'Pea',
+  },
+  {
+    hues: [
+      '#AEECCD',
+      '#69A197',
+      '#136F63',
+      '#0B3D36',
+    ],
+    name: 'Mint',
+  },
+  {
+    hues: [
+      '#94DADB',
+      '#74A4BC',
+      '#717EC3',
+      '#4A4E69',
+    ],
+    name: 'Sky',
+  },
+  {
+    hues: [
+      '#17BEBB',
+      '#2C8C99',
+      '#326771',
+      '#222831',
+    ],
+    name: 'Sea',
+  },
+];
+
+// export default {
+//   pullRandom,
+//   luck,
+//   verbs,
+//   actions,
+//   groups,
+//   feelings,
+//   colors,
+// };
