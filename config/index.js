0805c4022d112ec2e17465f7b29115d8553218c3
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-04-13 09:33:29 -0500

    update build repo

--- config/index.js
+++ config/index.js
@@ -50,10 +50,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../fortune-live/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../fortune-live'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
