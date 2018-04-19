167faa18c7ddb4cf67235cddd88061c472a2f23c
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-04-19 10:29:03 -0500

    remove routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
 import Fortune from '@/components/Fortune';
 import FortuneBack from '@/components/FortuneBack';
 import FortuneFront from '@/components/FortuneFront';
-import EmblemGood from '@/components/EmblemGood';
-import EmblemNormal from '@/components/EmblemNormal';
 
 Vue.use(Router);
 
@@ -28,21 +25,6 @@ export default new Router({
       name: 'FortuneFront',
       component: FortuneFront,
       props: true,
-    },
-    {
-      path: '/HW',
-      name: 'HelloWorld',
-      component: HelloWorld,
-    },
-    {
-      path: '/EG',
-      name: 'EmblemGood',
-      component: EmblemGood,
-    },
-    {
-      path: '/EN',
-      name: 'EmblemNormal',
-      component: EmblemNormal,
     },
   ],
 });
